fb8f55ab41e1fd9f38dc5539ea90b0d50f1734cc
ameedasmah1 <ameed.a@averotech.com>
2022-04-04 11:32:39 +0300

    modify home page with custome Fonts

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -8,8 +8,8 @@
         justify-center
         lg:px-8
         xl:px-0
-        text-4xl
-        font-extrabold
+        text-[38px]
+        font-shamelBold
         tracking-tight
         text-gray-900
         pb-8
@@ -18,7 +18,9 @@
       الفئات
     </h2>
 
-    <div class="grid mx-6 sm:mx-0  md:grid-cols-3 md:gap-x-6 grid-cols-1 gap-y-6">
+    <div
+      class="grid mx-6 sm:mx-0 md:grid-cols-3 md:gap-x-6 grid-cols-1 gap-y-6"
+    >
       <div
         class="
           bg-white
@@ -31,7 +33,7 @@
           items-center
           border-[#e424532e]
           text-[28px]
-          font-bold
+          font-shamelBold
         "
       >
         <MuslimHijab />
@@ -49,7 +51,7 @@
           items-center
           border-[#e424532e]
           text-[28px]
-          font-bold
+          font-shamelBold
         "
       >
         <muslim-dress />
@@ -67,37 +69,97 @@
           items-center
           border-[#e424532e]
           text-[28px]
-          font-bold
+          font-shamelBold
         "
       >
         <MuslimRobe />
         <h2>جلابيب</h2>
       </div>
     </div>
-    <!-- <div class="grid grid-cols-2 gap-10 ">
-      <div >
-        <div class=" ">
-          <h2 class="text-right text-[32px] font-bold border-b-8 border-[#F7A9E1]">توصيل خلال 24 ساعه</h2>
+    <div
+      class="
+        border-[1px]
+        bg-[#FADBDA]
+        border-[#FADBDA]
+        rounded-[20px]
+        mt-24
+        flex flex-row flex-wrap
+        justify-center
+        xl:justify-between
+        items-center
+        max-h-[490px]
+        xl:max-h-[650px]
+        pb-14
+      "
+    >
+      <div class="pl-10 relative hidden xl:flex">
+        <CustomCircular class="-mt-32"/>
+        <div
+          class="
+            absolute
+            top-[7%]
+            left-[33%]
+            flex flex-col
+            justify-center
+            items-center
+          "
+        >
+          <p class="text-sm text-[#201A3C] font-shamelnormal">زادك في الدنيا</p>
+          <p class="text-[40px] text-[#201A3C] font-bold">ما جديدنا#</p>
+          <p class="text-[17px] text-[#201A3C] font-shamelnormal">سجلي بياناتك</p>
+          <p class="text-[17px] text-[#201A3C] font-shamelnormal pb-2">
+            وانضمي لعائلة زاد
+          </p>
+          <div class="px-2 pt-2 pb-3 space-y-1">
+            <a
+              href="#"
+              class="
+                bg-[#201A3C]
+                text-white
+                block
+                px-3
+                py-3
+                pt-4
+                rounded-[10px]
+                text-[16px]
+                font-bold
+              "
+              aria-current="page"
+              >انضمي الان</a
+            >
+          </div>
         </div>
-        <h2 class="text-right text-[18px] font-normal">افضل خدمات التوصيل</h2>
       </div>
-      <p class="text-left text-[32px] font-bold">تسوق على حسب السعر</p>
-    </div> -->
+      <div class="pr-20 -mt-8 xl:mt-0 pb-0 xl:pt-14 xl:pb-28">
+        <!-- <img class="shadoww" :src="HijabImage" /> -->
+        <BoxShadow />
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+</style>
 
 <script>
 import MuslimHijab from "../assets/icons/MuslimHijab.vue";
 import MuslimDress from "../assets/icons/MuslimDress.vue";
 import MuslimRobe from "../assets/icons/MuslimRobe.vue";
+import CustomCircular from "../assets/icons/CustomCircular.vue";
+import BoxShadow from "../assets/icons/ImageBoxShadow.vue";
+
+const HijabImage =
+  "https://media.discordapp.net/attachments/938405759996276806/960167429508591626/image_9.png";
 export default {
   data() {
-    return {};
+    return { HijabImage };
   },
   components: {
     MuslimHijab,
     MuslimDress,
     MuslimRobe,
+    CustomCircular,
+    BoxShadow,
   },
 };
 </script>
---
+++ src/components/Sliedr.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="max-w-7xl mx-auto container px-6 lg:px-8 pt-16">
+    <div
+      class="
+        text-[#201A3C] text-[36px]
+        font-shamelBold
+        flex
+        justify-center
+        items-center
+        relative
+        mb-6
+      "
+    >
+      <WhatsApp class="absolute left-4 -top-5" />
+      <p>رأي زبونتنا</p>
+    </div>
+    <div
+      class="
+        flex flex-col
+        items-center
+        justify-center
+        border-2 border-[#E424532E]
+        min-h-[450px]
+        rounded-[20px]
+        shadow-2xl
+      "
+    >
+      <p class="text-[#CC9933] text-[27px] font-shamelBold">امل عماد</p>
+      <p
+        class="text-[16px] text-[#201A3C] max-w-[400px] text-center font-shamelnormal"
+      >
+        هناك حقيقة مثبته منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي
+        القارئ عن التركيز على الشكل الخارجي للنص او شكل توضع الفقرات في
+        الصفحةالتي يقرأها
+      </p>
+      <div class="flex flex-row items-center justify-center pt-8">
+      <span class="h-[17px] w-[17px] bg-[white] rounded-[50%] border-[#201A3C] border-[1px] "></span>
+      <span class="h-[17px] w-[17px]  rounded-[50%] bg-[#CC9933] mx-2"></span>
+      <span class="h-[17px] w-[17px] bg-[white] rounded-[50%] border-[#201A3C] border-[1px]"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import WhatsApp from "../assets/icons/Whatsapp.vue";
+export default {
+  setup() {
+    return {};
+  },
+  components: { WhatsApp },
+};
+</script>
