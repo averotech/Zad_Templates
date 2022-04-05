5bb3e0d57ceaab781e345ac5c1d30dee9865c759
ameedasmah1 <ameed.a@averotech.com>
2022-04-05 14:35:36 +0300

    adding Steeper

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -1,16 +1,17 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <nav aria-label="Progress" class="my-11 mx-11">
-    <ol role="list" class="flex items-center">
+    <ol role="list" class="flex flex-row-reverse items-center">
       <li v-for="(step, stepIdx) in steps" :key="step.name"  :class="[stepIdx !== steps.length - 1 ? 'pr-8 sm:pr-20' : '', 'relative']">
         <template  v-if="step.status === 'complete'">
-          <div class="absolute inset-0 flex items-center" aria-hidden="true">
+          <div class="absolute inset-0 flex  items-center" aria-hidden="true">
             <div class="" />
-            <span>{{step.descraption}}</span>
+            <span class="text-[#CC9933]">{{step.descraption}}</span>
           </div>
-          <a href="#" class="relative w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full hover:bg-indigo-900">
-            <CheckIcon class="w-5 h-5 text-white" aria-hidden="true" />
-            <span class="sr-only">{{ step.name }}</span>
+          <a href="#" class="relative w-8 h-8 flex items-center justify-center bg-[#CC9933] rounded-full">
+            <!-- <CheckIcon class="w-5 h-5 text-white" aria-hidden="true" /> -->
+            <p class="pt-1">{{step.step}}</p>
+            <span class="sr-only ">{{ step.name }}</span>
           </a>
         </template>
         <template v-else-if="step.status === 'current'" condition="step.status === 'current'">
@@ -40,10 +41,10 @@
 import { CheckIcon } from '@heroicons/vue/solid'
 
 const steps = [
-  { name: 'Step 1', href: '#',descraption:"حقيبة التسوق", status: 'complete' },
-  { name: 'Step 2', href: '#',descraption:"تأكيد الطلب", status: 'current' },
-  { name: 'Step 3', href: '#',descraption:"دفع", status: 'upcoming' },
-  { name: 'Step 4', href: '#',descraption:"تم الطلب", status: 'upcoming' },
+  { name: 'Step 1',step:1, href: '#',descraption:"حقيبة التسوق", status: 'complete' },
+  { name: 'Step 2',step:3, href: '#',descraption:"تأكيد الطلب", status: 'current' },
+  { name: 'Step 3',step:4, href: '#',descraption:"دفع", status: 'upcoming' },
+  { name: 'Step 4',step:5, href: '#',descraption:"تم الطلب", status: 'upcoming' },
 ]
 
 export default {
